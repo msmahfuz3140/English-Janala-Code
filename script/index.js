a120a138ad46084d9636a7120534d8cafc9eb06d
msmahfuz3140 <mdmahfuzulhaque3140@gmail.com>
2026-03-05 15:02:23 +0600

    design word container and responsive and dynamically add word

--- script/index.js
+++ script/index.js
@@ -4,18 +4,45 @@ const loadLessons = () => {
         .then(json => displayLessons(json.data))
 }
 
+const loadLevelWord = (id) => {
+    const url = `https://openapi.programming-hero.com/api/level/${id}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayLevelWord(data.data))
+}
+
+const displayLevelWord = words => {
+    const wordContainer = document.getElementById("word-container");
+    wordContainer.innerHTML = "";
+
+    words.forEach(word => {
+        const card = document.createElement("div");
+        card.innerHTML = `
+        <div class="bg-white rounded-xl shadow-sm text-center py-15 px-5 space-y-5">
+            <h2 class="text-2xl font-bold">${word.word}</h2>
+            <p class="font-semibold">Meaning /Pronounciation</p>
+            <div class="text-2xl font-medium font-bangla">"${word.meaning} / ${word.pronunciation}"</div>
+            <div class="flex justify-between items-center">
+                <button class="btn bg-sky-50 rounded-md hover:bg-sky-400"><i class="fa-solid fa-circle-info"></i></button>
+                <button class="btn bg-sky-50 rounded-md hover:bg-sky-400"><i class="fa-solid fa-volume-high"></i></button>
+            </div>
+        </div>
+        `
+        wordContainer.append(card);
+    })
+}
+
 const displayLessons = lessons => {
     // 1. get the container & empty
     const levelContainer = document.getElementById("level-container");
     levelContainer.innerHTML = "";
     // 2. get into every lessons
     for (let lesson of lessons) {
-        console.log(lesson);
         //     3. create Element
 
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML = `
-        <button class="btn btn-outline btn-primary">
+        <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary">
         <i class="fa-solid fa-book-open"></i>Lesson-${lesson.level_no}
         </button>
         `
